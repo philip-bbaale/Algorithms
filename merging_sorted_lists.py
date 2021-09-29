f2b24fbdaed15d4dfa0ffdb77b79d96b87e1a111
Philip Namugera Bbaale <namugera.philip@gmail.com>
2021-09-29 10:01:19 +0300

    adding Description

--- merging_sorted_lists.py
+++ merging_sorted_lists.py
@@ -23,3 +23,24 @@ l2 = [2,5,6, 7, 8]
 
 res = mergeTwoLists(l1, l2)
 print(res)
+
+
+"""
+Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+ 
+
+Example 1:
+
+
+Input: l1 = [1,2,4], l2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: l1 = [], l2 = []
+Output: []
+Example 3:
+
+Input: l1 = [], l2 = [0]
+Output: [0]
+"""
