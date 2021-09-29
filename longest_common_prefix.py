628ccefdfde81eb5b96398afe76a895844cc9438
Philip Namugera Bbaale <namugera.philip@gmail.com>
2021-09-29 09:51:36 +0300

    Longest Common Preffix

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -1,5 +1,5 @@
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
+    def longestCommonPrefix(self, strs):
         new = list(zip(*strs))
         prefix = ""
         if len(strs) == 0:
